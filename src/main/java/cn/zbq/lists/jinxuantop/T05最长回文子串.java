366cda3f2666ed5035712f467ba2cd3a5e26266d
zbq <aa@mmm.com>
2020-04-28 15:47:48 +0800

    最长回文子串

--- src/main/java/cn/zbq/lists/jinxuantop/T05最长回文子串.java
+++ src/main/java/cn/zbq/lists/jinxuantop/T05最长回文子串.java
@@ -58,4 +58,45 @@ public class T05最长回文子串 {
         }
         return result;
     }
+
+    // 官方题解
+    public static String longestPalindrome2(String s) {
+        if (s == null || s.length() < 1) {
+            return "";
+        }
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            // 长度为奇数
+            int l1 = lookUpMaxLenRange(s, i, i);
+            // 长度为偶数
+            int l2 = lookUpMaxLenRange(s, i, i + 1);
+            int l = Math.max(l1, l2);
+            if (l > end - start) {
+                // 计算起始和结束
+                // 长度从1开始计算，但索引是从0开始，所以长度减去1，然后左右各一半
+                // <--
+                start = i - (l - 1) / 2;
+                // 向下取整
+                // -->
+                end = i + l / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    // 寻找最长范围
+    private static int lookUpMaxLenRange(String s, int leftIndex, int rightIndex) {
+        int left = leftIndex, right = rightIndex;
+        // 从给定位置开始寻找，每次前外侧走一个单位
+        // 左侧不能小于0，右侧不能大于字符串的长度，并且左右两侧字符要相等
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        // 左右不满足才会从循环中结束，此时会比符合条件的数据长度多1
+        // fgaba
+        // 01234
+        // 满足条件时 right = 2，left=4，但从循环中结束时，right =1，left=5，此时符合条件的长度为3，left-right-1=3
+        return right - left - 1;
+    }
 }
